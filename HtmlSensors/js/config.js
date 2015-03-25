78920b5838f7fdaa0dcc6575ee3830cbddb1a173
Victor Cazacov <cazacov.git@gmail.com>
2015-03-25 23:50:36 +0100

    Demo read accelerometer and compass in HTML

---
+++ HtmlSensors/js/config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+  baseUrl: "js",
+  shim: {
+
+  },
+  paths: {
+    sprintf: "lib/sprintf/src/sprintf",
+    requirejs: "lib/requirejs/require",
+    angular: "lib/angular/angular"
+  }
+});
